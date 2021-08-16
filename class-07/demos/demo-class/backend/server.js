ddcfe7429fed4671d66756e89eeb01b2747ae378
RazanAl-Quran <r.alquran@ltuc.com>
2021-08-16 14:17:26 +0300

    class07 done

---
+++ class-07/demos/demo-class/backend/server.js
@@ -0,0 +1,53 @@
+'use strict';
+
+const express = require('express');
+require('dotenv').config();
+const cors = require('cors')
+
+const pokeData = require('./assets/poke.json')
+
+const server = express();
+const PORT = process.env.PORT;
+server.use(cors());
+
+
+
+//localhost:3001/
+server.get('/',(req,res)=>{
+    res.send('home route')
+})
+
+//localhost:3001/test
+server.get('/test',(request,response)=>{
+    response.send('your server is working')
+})
+
+// localhost:3001/getShoppingData
+server.get('/getShoppingData',(req,res)=>{
+    let shoppingArray = ['shoes','bag','toys'];
+    res.status(200).send(shoppingArray);
+})
+
+
+//localhost:3001/getDataFromPoke?pokeName=bulbasur
+server.get('/getDataFromPoke',(req,res)=>{
+    // res.send(pokeData.results)
+    console.log(req.query);
+    let pokemonName = req.query.pokeName;
+    let pokeInfo = pokeData.results.find(pokemon=>{
+        if(pokemon.name.toLowerCase()===pokemonName) {
+            return pokemon;
+        }
+    })
+    res.send(pokeInfo);
+})
+
+server.get('*',(req,res)=>{
+    res.status(404).send('not found')
+})
+
+
+
+server.listen(PORT,()=>{
+    console.log(`Listning on PORT ${PORT}`)
+})
---
+++ class-07/demos/demo-class/frontend/src/App.js
@@ -0,0 +1,43 @@
+import axios from 'axios';
+import React from 'react'
+
+class App extends React.Component {
+
+  constructor(props) {
+    super(props);
+    this.state = {
+      pokeName: '',
+      pokeInfo: {}
+    }
+  }
+
+  getPokeData = async (e) =>{
+    e.preventDefault();
+    console.log(e.target.poke.value)
+    await this.setState({
+      pokeName:e.target.poke.value
+    })
+    // http://localhost:3001/getDataFromPoke?pokeName=pidgey
+    let pokeDataResult = await axios.get(`${process.env.REACT_APP_SERVER_LINK}/getDataFromPoke?pokeName=${this.state.pokeName}`)
+    console.log('kkkkkkkkk',pokeDataResult.data)
+    await this.setState({
+      pokeInfo: pokeDataResult.data
+    })
+  }
+
+  render() {
+    return (
+      <div>
+        <h2>Pokemon App</h2>
+        <form onSubmit={this.getPokeData}>
+          <input type="text" name='poke' />
+          <button> Get Poke Data</button>
+        </form>
+        <p>{this.state.pokeInfo.name}</p>
+        <p>{this.state.pokeInfo.url}</p>
+      </div>
+    )
+  }
+}
+
+export default App
